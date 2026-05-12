4410ed587229689bc51d32b2cea64c5fdbd5f689
Fabio Fernandez <kingofnoobs845@gmail.com>
2026-05-12 16:20:15 -0400

    feat: two sum

--- main.cpp
+++ main.cpp
@@ -1,30 +1,38 @@
-#include <vector>
 #include <iostream>
-#include <string>
-#include <cctype>
-using namespace std;
+#include <vector>
 
+using namespace std;
+// beecrowd | 1238
+// combiner
+
+vector<int> solve(vector<int> nums, int target)
+{
+    int left = 0;
+    // int results[2]{0};
+    vector<int> results;
+
+    while (left < nums.size())
+    {
+        for (int right = left + 1; right < nums.size(); right++)
+        {
+            if (nums[left] + nums[right] == target)
+            {
+                results.push_back(left);
+                results.push_back(right);
+            }
+        }
+        left++;
+    }
+    for (int i = 0; i < results.size(); i++)
+    {
+        cout << results[i] << " ";
+    }
+    return results;
+}
 int main()
 {
-    int cases;
-    cin >> cases;
-    cin.ignore();
-    string str, aux = "";
-    while (cases--)
-    {
-        aux = "";
-
-        getline(cin, str);
-        int mid = str.size() / 2;
-        for (int i = mid - 1; i >= 0; i--)
-        {
-            aux += str[i];
-        }
-        for (int i = str.size() - 1; i >= mid; i--)
-        {
-            aux += str[i];
-        }
-        cout << aux << endl;
-    }
+    vector<int> nums{2, 7, 11, 15};
+    int target = 9;
+    solve(nums, target);
     return 0;
 }
