c9a2dd30399f437867a3ad4e3cf5a3402cb08d5b
Fabio Fernandez <kingofnoobs845@gmail.com>
2026-05-31 06:14:44 -0400

    feat: progress Remove Nth Node From End of List with LinkesList

--- main.cpp
+++ main.cpp
@@ -2,67 +2,78 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-// 4SUM -> Leetcode
-int main()
+// Remove Nth Node From End of List -> Leetcode
+
+class Node
 {
-    vector<int> nums = {-3, -2, -1, 0, 0, 1, 2, 3};
-    sort(nums.begin(), nums.end());
-    int target = 0;
-    vector<vector<int>> result;
-    if (nums.size() < 4)
-        return {};
-    for (int i = 0; i < nums.size() - 3; i++)
+public:
+    int value;
+    Node *next;
+    Node(int data)
     {
+        value = data;
+        next = nullptr;
+    };
+};
+class ListNode
+{
+public:
+    Node *head;
 
-        if (i > 0 && nums[i] == nums[i - 1])
-            continue;
-        for (int j = i + 1; j < nums.size() - 2; j++)
-        {
-
-            if (j > i + 1 && nums[j] == nums[j - 1])
-                continue;
-            int left = j + 1, right = nums.size() - 1;
-
-            while (left < right)
-            {
-                long long sum =
-                    1LL * nums[i] +
-                    nums[j] +
-                    nums[left] +
-                    nums[right];
-                if (sum == target)
-                {
-                    result.push_back({nums[i],
-                                      nums[j],
-                                      nums[left],
-                                      nums[right]});
-
-                    left++;
-                    right--;
-
-                    while (left < right &&
-                           nums[left] == nums[left - 1])
-                    {
-                        left++;
-                    }
-
-                    while (left < right &&
-                           nums[right] == nums[right + 1])
-                    {
-                        right--;
-                    }
-                }
-                else if (sum > target)
-                {
-                    right--;
-                }
-                else
-                {
-                    left++;
-                }
-            }
-        }
+    ListNode()
+    {
+        head = nullptr;
     }
 
+    void insert(int value)
+    {
+        Node *newNode = new Node(value);
+        if (head == nullptr)
+        {
+            head = newNode;
+            return;
+        }
+        Node *temp = head;
+        while (temp->next != nullptr)
+            temp = temp->next;
+        temp->next = newNode;
+    }
+    void print()
+    {
+        Node *temp = head;
+        while (temp != nullptr)
+        {
+            cout << temp->value << " ";
+            temp = temp->next;
+        }
+    }
+    int size_linkes_list()
+    {
+        int _size = 0;
+        Node *temp = head;
+        while (temp != nullptr)
+        {
+            _size++;
+            temp = temp->next;
+        }
+        return _size;
+    }
+
+    void remove_data(int position)
+    {
+    }
+};
+int main()
+{
+    ListNode list;
+    list.insert(1);
+    list.insert(2);
+    list.insert(3);
+    list.insert(4);
+    list.insert(5);
+    int remove_value = 2;
+
+    // list.print();
+    cout << list.size_linkes_list();
     return 0;
 };
