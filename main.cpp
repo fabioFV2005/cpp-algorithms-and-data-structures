8578353ce748073394fe59a5d605bba2b4bed9ca
Fabio Fernandez <kingofnoobs845@gmail.com>
2026-08-06 17:20:58 -0400

    feat: Initial approach of 283 - move Zeros

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
 int main() {
-    int val = 0;
-    cout << val <<" hello world!" <<endl ;
+    vector<int> nums = {0, 1, 0, 3, 12};
+    int write = 0, read = 0;
+    while (read < nums.size()) {
+        if (nums[read] == 0) {
+            for (int i = read; i < nums.size(); i++) {
+                if (nums[i] != 0) {
+                    nums[read] = nums[i];
+                    nums[i] = 0;
+                    break;
+                }
 
+            }
+        }
+        read++;
 
+    }
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i] << " ";
+    }
     return 0;
 }
