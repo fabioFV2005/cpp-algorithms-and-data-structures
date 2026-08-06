0f2fa6342a50738f337ff0977c184afbfafcd7d1
Fabio Fernandez <kingofnoobs845@gmail.com>
2026-08-06 16:18:10 -0400

    feat : initializing 283 Move Zeroes

--- main.cpp
+++ main.cpp
@@ -1,17 +1,9 @@
-class Solution {
-public:
-    bool increasingTriplet(vector<int>& nums) {
-        int max1 = INT_MAX;
-        int max2 = INT_MAX;
+#include <iostream>
+using namespace std;
+int main() {
+    int val = 0;
+    cout << val <<" hello world!" <<endl ;
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] <= max1)
-                max1 = nums[i];
-            else if (nums[i] <= max2)
-                max2 = nums[i];
-            else
-                return true;
-        }
-        return false;
-    }
-};
+
+    return 0;
+}
