d8125c85e01c58e3a84c27a82bd40ca9c5511181
Fabio Fernandez <kingofnoobs845@gmail.com>
2026-09-05 13:16:10 -0400

    feat: slice the files

---
+++ strings/WayTooLoongWords.h
@@ -0,0 +1,13 @@
+//
+// Created by Fabio on 05/09/2026.
+//
+
+#ifndef LOGIC_WAYTOOLOONGWORDS_H
+#define LOGIC_WAYTOOLOONGWORDS_H
+
+
+class WayTooLoongWords {
+};
+
+
+#endif //LOGIC_WAYTOOLOONGWORDS_H
