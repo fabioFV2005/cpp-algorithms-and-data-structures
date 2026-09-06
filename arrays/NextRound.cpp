0d50a5ffd4ba9cd3418da76cf33d086b3f078048
Fabio Fernandez <kingofnoobs845@gmail.com>
2026-09-06 11:11:33 -0400

    feat: slicing files

---
+++ arrays/NextRound.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Fabio on 06/09/2026.
+//
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int main() {
+    int n, k, count = 0;
+    cin >> n >> k;
+    int participants[n];
+    for (int i = 0; i < n; i++) {
+        cin >> participants[i];
+    }
+    int score = participants[k-1];
+    for (int i = 0; i < n; i++) {
+        if (participants[i] < 1) continue;
+        if (participants[i] >= score) count++;
+    }
+    cout << count << endl;
+    return 0;
+}
