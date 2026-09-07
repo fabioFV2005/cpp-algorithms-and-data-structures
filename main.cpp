45e1cbcee8c7ba235985e03c2c85c29ea6f1077d
Fabio Fernandez <kingofnoobs845@gmail.com>
2026-09-06 23:02:07 -0400

    feat: solve domino piling

--- main.cpp
+++ main.cpp
@@ -4,17 +4,9 @@ using namespace std;
 
 
 int main() {
-    int n, k, count = 0;
-    cin >> n >> k;
-    int participants[n];
-    for (int i = 0; i < n; i++) {
-        cin >> participants[i];
-    }
-    int score = participants[k-1];
-    for (int i = 0; i < n; i++) {
-        if (participants[i] < 1) continue;
-        if (participants[i] >= score) count++;
-    }
-    cout << count << endl;
+    int m, n;
+    cin >> m >> n;
+
+    cout << ((m*n) / (2))  << endl;
     return 0;
 }
