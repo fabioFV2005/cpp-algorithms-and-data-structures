e2a8da610f7603d82ecc416a922e6f05109e19f8
Fabio Fernandez <kingofnoobs845@gmail.com>
2026-09-06 23:49:38 -0400

    chore: organize exercise files

---
+++ Matrix/BeautifulMatrix.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Fabio on 06/09/2026.
+//
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int main() {
+    int x,y;
+    int matrix[5][5] = {0};
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            cin >> matrix[i][j];
+            if (matrix[i][j] == 1) {
+                x = i;
+                y = j;
+            }
+        }
+    }
+    cout << abs(y - 2) + abs(x - 2) <<endl;
+    return 0;
+}
