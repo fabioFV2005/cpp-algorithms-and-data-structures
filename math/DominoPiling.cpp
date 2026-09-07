8fbeec1531cf374efba4ea7ebb07eb6b275c73f8
Fabio Fernandez <kingofnoobs845@gmail.com>
2026-09-06 23:04:31 -0400

    chore: organize exercise files

---
+++ math/DominoPiling.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Fabio on 06/09/2026.
+//
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+
+    cout << ((m*n) / (2))  << endl;
+    return 0;
+}
