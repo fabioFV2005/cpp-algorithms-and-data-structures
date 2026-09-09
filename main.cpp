bd2a110e39f43ba8f6ffb7bf30a82667835df26d
Fabio Fernandez <kingofnoobs845@gmail.com>
2026-09-08 23:34:58 -0400

    feat: Boy or Girl solved

--- main.cpp
+++ main.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
-#include <cctype>
+#include <vector>
 using namespace std;
 
-int solve(string s1, string s2) {
-    for (int i = 0; i < s1.length(); i++) {
-        if (tolower(s1[i]) < tolower(s2[i])) return -1;
-        if ( tolower(s2[i]) < tolower(s1[i])) return 1;
-    }
-    return 0;
+bool is_odd(int number) {
+    return (number % 2 == 0);
 }
 int main() {
-
-    string s1, s2;
-    cin >>  s1 >> s2;
-    cout << solve(s1, s2)<< endl;
+    vector<int> freq(26, 0);
+    string str;
+    int count = 0;
+    cin >> str;
+    for (char c : str) {
+        freq[c]++;
+    }
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] != 0) {
+            count++;
+        }
+    }
+    is_odd(count)? cout << "CHAT WITH HER!" << endl : cout << "IGNORE HIM!" << endl;
     return 0;
 }
