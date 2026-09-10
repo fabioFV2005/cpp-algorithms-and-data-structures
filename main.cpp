747065f432cf3f98d68288da73e863b6f5b8bed3
Fabio Fernandez <kingofnoobs845@gmail.com>
2026-09-09 22:21:42 -0400

    feat: helpful maths initial solution naive approach

--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
 
-bool is_odd(int number) {
-    return (number % 2 == 0);
-}
+
 int main() {
-    vector<int> freq(26, 0);
-    string str;
-    int count = 0;
-    cin >> str;
-    for (char c : str) {
-        freq[c]++;
-    }
-    for (int i = 0; i < 26; i++) {
-        if (freq[i] != 0) {
-            count++;
+    string s;
+    cin >> s;
+    s.erase(
+        remove_if(s.begin(), s.end(), [](char c) {
+            return c == '+';
+        } ), s.end()
+        );
+    sort(s.begin(), s.end());
+    for (int i = 0; i < s.size(); i++) {
+        cout << s[i];
+        if (i != s.size() - 1) {
+            cout << '+';
         }
     }
-    is_odd(count)? cout << "CHAT WITH HER!" << endl : cout << "IGNORE HIM!" << endl;
+    cout << endl;
     return 0;
 }
