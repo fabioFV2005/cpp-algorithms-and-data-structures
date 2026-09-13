819d8afc80b58c02d1b7f559518e3f9733e631b7
Fabio Fernandez <kingofnoobs845@gmail.com>
2026-09-12 22:15:37 -0400

    chore: organize files

---
+++ math/Elephant.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Fabio on 12/09/2026.
+//
+#include <iostream>
+#include <cctype>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int x, operation=0;
+    cin >> x;
+    operation = (x+4)/5;
+    cout << operation;
+    return 0;
+}
