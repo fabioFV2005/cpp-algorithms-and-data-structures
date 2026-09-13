7221a2cf54fd5149e0e5ca1a3b2711ebf250bc9a
Fabio Fernandez <kingofnoobs845@gmail.com>
2026-09-13 00:40:21 -0400

    chore: organize files

---
+++ strings/StonesOnTheTable.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Fabio on 13/09/2026.
+//
+#include <iostream>
+#include <cctype>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    int read=1, write=0, count=0;
+    string s;
+    cin >> n;
+    cin >> s;
+    for (int i = 0; i < n; i++) {
+        if (s[i] == s[i+1]) {
+            count++;
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
