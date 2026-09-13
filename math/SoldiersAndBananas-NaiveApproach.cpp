573f2b6357a7358b60c26f8e13523ec5df4aebf4
Fabio Fernandez <kingofnoobs845@gmail.com>
2026-09-13 12:27:13 -0400

    feat: 546A - soldiers and bananas solved - Math approach

--- math/SoldiersAndBananas-NaiveApproach.cpp
+++ math/SoldiersAndBananas-NaiveApproach.cpp
@@ -1,8 +1,18 @@
+//
+// Created by Fabio on 13/09/2026.
+//
 #include <iostream>
 #include <cctype>
 #include <algorithm>
 using namespace std;
 
+int solve(int w, int k) {
+    int sum = 0;
+    for (int i = w; i >=1; i--)  {
+        sum += i*k;
+    }
+    return sum;
+}
 int main() {
 
     int k,n,w;
@@ -10,7 +20,7 @@ int main() {
     // w is the number of bananas he wants to buy,
     // n is the amount of money he has.
     cin >> k >> n >> w;
-    int total = k * w * (w + 1) / 2;
+    int total = solve(w,k);
     (total - n) > 0 ? cout << total - n << endl: cout << 0 << endl;
     return 0;
 }
