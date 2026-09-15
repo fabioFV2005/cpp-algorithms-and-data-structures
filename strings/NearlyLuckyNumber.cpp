04c1224fcac6a7e86055dee87314096deca7122b
Fabio Fernandez <kingofnoobs845@gmail.com>
2026-09-14 22:48:40 -0400

    chore: organize files

---
+++ strings/NearlyLuckyNumber.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Fabio on 14/09/2026.
+//
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool NearlyLuckyNumber(char digit) {
+    return digit == '4' || digit == '7';
+}
+
+int main() {
+    int count = 0;
+    string digits;
+    cin >> digits;
+    for (int i = 0; i < digits.length(); i++) {
+        if (NearlyLuckyNumber(digits[i])) count++;
+    }
+    if (count == 4 || count == 7) cout << "YES" << endl;
+    else cout << "NO" << endl;
+    return 0;
+}
