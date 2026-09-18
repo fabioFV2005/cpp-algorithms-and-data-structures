6231e3555198bedd6959cb0df9d76c8bf97249e6
Fabio Fernandez <kingofnoobs845@gmail.com>
2026-09-18 14:24:09 -0400

    feat: 41A - Translation solution

--- main.cpp
+++ main.cpp
@@ -1,26 +1,16 @@
 #include <iostream>
-#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-
+bool solve(string s, string t) {
+    reverse(t.begin(), t.end());
+    return s == t;
+}
 int main() {
-    int n, A, D;
-    string s;
-    cin >> n;
-    cin >> s;
-    for (int i = 0; i < n; i++) {
-        if (s[i]== 'A') {
-            A++;
-        }else if (s[i]== 'D') {
-            D++;
-        }
-    }
-    if (A>D) {
-        cout << "Anton" << endl;
-    }else if (A<D){
-        cout << "Danik" << endl;
-    }else {
-        cout << "Friendship" << endl;
-    }
+    string s, t;
+    cin >> s >> t;
+    if (solve(s, t))cout << "YES" << endl;
+    else cout << "NO" << endl;
     return 0;
 }
