e3494afc1f91b971ff318350ae72e92e2e22b088
Fabio Fernandez <kingofnoobs845@gmail.com>
2026-09-18 14:38:51 -0400

    feat 677A - Vanya and Fence solved

--- main.cpp
+++ main.cpp
@@ -3,14 +3,13 @@
 #include <algorithm>
 using namespace std;
 
-bool solve(string s, string t) {
-    reverse(t.begin(), t.end());
-    return s == t;
-}
 int main() {
-    string s, t;
-    cin >> s >> t;
-    if (solve(s, t))cout << "YES" << endl;
-    else cout << "NO" << endl;
-    return 0;
+    int n, h, a, count=0;
+    cin >> n >> h;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        if (a > h) count+=2;
+        else count+=1;
+    }
+    cout << count << endl;
 }
