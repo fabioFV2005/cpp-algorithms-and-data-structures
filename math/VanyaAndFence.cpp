d0c0946f13e4370245739020881f90416f4fdf02
Fabio Fernandez <kingofnoobs845@gmail.com>
2026-09-18 14:39:18 -0400

    feat: organizing files

---
+++ math/VanyaAndFence.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Fabio on 18/09/2026.
+//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, h, a, count=0;
+    cin >> n >> h;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        if (a > h) count+=2;
+        else count+=1;
+    }
+    cout << count << endl;
+}
